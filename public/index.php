aac9b435777f168e3055b2010ce685572b6f2ed0
Jahir Hossain <j.hossain@reply.de>
2023-05-05 08:31:43 +0200

    Add request class

--- public/index.php
+++ public/index.php
@@ -1,8 +1,11 @@
-<?php declare(strict_types=1)
+<?php declare(strict_types=1);
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 // receive request
+use Jahir\Framework\Http\Request;
+
+$request = Request::createFromGlobals();
 
 // perform some logic
 
