c03fec49291bca5977475cfed5cb2659ac5a601d
Jahir Hossain <j.hossain@reply.de>
2023-05-08 16:35:50 +0200

    Add Twig environment

--- config/services.php
+++ config/services.php
@@ -1,5 +1,6 @@
 <?php
 
+use Jahir\Framework\Controller\AbstractController;
 use Jahir\Framework\Http\Kernel;
 use Jahir\Framework\Routing\Router;
 use Jahir\Framework\Routing\RouterInterface;
@@ -8,9 +9,13 @@ use League\Container\Argument\Literal\StringArgument;
 use League\Container\Container;
 use League\Container\ReflectionContainer;
 use Symfony\Component\Dotenv\Dotenv;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 $dotEnv = new Dotenv();
 $dotEnv->load(BASE_PATH.'/.env');
+
+$templatePath = BASE_PATH.'/templates';
 
 $container = new Container();
 
@@ -27,5 +32,16 @@ $container->extend(RouterInterface::class)
 $container->add(Kernel::class)
     ->addArgument(RouterInterface::class)
     ->addArgument($container);
+
+$container->addShared('filesystem-loader', FilesystemLoader::class)
+    ->addArgument(new StringArgument($templatePath));
+
+$container->addShared('twig', Environment::class)
+    ->addArgument('filesystem-loader');
+
+$container->add(AbstractController::class);
+
+$container->inflector(AbstractController::class)
+    ->invokeMethod('setContainer', [$container]);
 
 return $container;
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
+use Jahir\Framework\Controller\AbstractController;
 use Jahir\Framework\Http\Response;
 
-class HomeController
+class HomeController extends AbstractController
 {
     public function index(): Response
     {
+        dd($this->container->get('twig'));
         $content = "<h1>Hello World</h1>";
         return new Response($content);
     }
