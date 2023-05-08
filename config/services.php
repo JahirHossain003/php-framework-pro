a80bc54baaab02a99e0467d780f75e37e0ae7620
Jahir Hossain <j.hossain@reply.de>
2023-05-08 08:32:21 +0200

    Add container alias

--- config/services.php
+++ config/services.php
@@ -2,4 +2,8 @@
 
 $container = new \League\Container\Container();
 
+$container->add(\Jahir\Framework\Routing\RouterInterface::class, \Jahir\Framework\Routing\Router::class);
+
+$container->add(\Jahir\Framework\Http\Kernel::class)->addArgument(\Jahir\Framework\Routing\RouterInterface::class);
+
 return $container;
--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 use Jahir\Framework\Http\Kernel;
 use Jahir\Framework\Http\Request;
-use Jahir\Framework\Routing\Router;
 
 define('BASE_PATH', dirname(__DIR__));
 
@@ -10,13 +9,9 @@ require_once BASE_PATH.'/vendor/autoload.php';
 
 $container = require BASE_PATH.'/config/services.php';
 
-dd($container);
-
 $request = Request::createFromGlobals();
 
-$router  = new Router();
-
-$kernel = new Kernel($router);
+$kernel = $container->get(Kernel::class);
 
 $response = $kernel->handle($request);
 
