995d14878c7331e66b0188275a49e18a453fec1d
Jahir Hossain <j.hossain@reply.de>
2023-05-12 17:00:56 +0200

    Setup session

---
+++ framework/tests/SessionTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Jahir\Framework\Tests;
+
+use Jahir\Framework\Session\Session;
+use PHPUnit\Framework\TestCase;
+
+class SessionTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        unset($_SESSION);
+    }
+
+    public function testSetAndGetFlashMessage() {
+        $session = new Session();
+        $session->setFlash('success', 'Great job!');
+        $session->setFlash('error', 'Bad job!');
+        $this->assertTrue($session->hasFlash('success'));
+        $this->assertTrue($session->hasFlash('error'));
+        $this->assertEquals(['Great job!'], $session->getFlash('success'));
+        $this->assertEquals(['Bad job!'], $session->getFlash('error'));
+        $this->assertEquals([], $session->getFlash('warning'));
+    }
+}
